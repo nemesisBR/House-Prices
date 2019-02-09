587e3cb5909100165a9effda8853ed9f2ee868be
Boby Robert <bobyrobert3@gmail.com>
2019-02-10 01:17:15 +0530

    Removed least correlated, filled null values, and columns with less unique values
    Removed columns whose correlation was less than zero.
    
    Rreplaced integer nan values with mean and string values with mode of respective columns
    
    Removed columns with less than 100 unique values

--- house_prices.py
+++ house_prices.py
@@ -4,44 +4,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def modify_data(dataset):
-    drop_column = ['Id','Alley', 'PoolQC', 'Fence', 'MiscFeature']
+    # Highly Blank Values & Correlation < Zero
+    drop_column = ['Id','Alley', 'PoolQC', 'Fence', 'MiscFeature','MSSubClass','OverallCond','BsmtFinSF2','LowQualFinSF','BsmtHalfBath','KitchenAbvGr','EnclosedPorch','MiscVal','YrSold']
     dataset.drop(drop_column, axis = 1, inplace = True)
-    dataset = dataset.dropna(axis = 0 , how='all')
-    
-    null = dataset.isnull().sum()                                    #Remove Columns 'Alley', 'PoolQC', 'Fence', 'MiscFeature'
+     
+    #Filling BLank Values, integer with mean & Rest with mode
+    null = dataset.isnull().sum()                                    
     nullColumns = null[null > 0].index.values.tolist()
-    describe = dataset[nullColumns].describe()                       # Only 3 columns to be filed with int values inplace of na
+    describe = dataset[nullColumns].describe()                       
     intNullColumns = describe.columns.values.tolist()
     
     for col in intNullColumns:
-        dataset[col].fillna(dataset[col].mean(),inplace = True)         #Integer null columns with mean
+        dataset[col].fillna(dataset[col].mean(),inplace = True)  #Integer null columns with mean
         nullColumns.remove(col)
     for col in nullColumns:
-        dataset[col] = dataset[col].str.encode('utf-8')
-        dataset[col].fillna(dataset[col].mode(),inplace = True)         #String null columns with mode
+        dataset[col].fillna(dataset[col].dropna().mode()[0],inplace = True) #String null columns with mode
     
-    return dataset
-
+    dataset.dropna(axis = 0 , how='all', inplace = True)
+    
+def remLeastUniqueColumns(dataset,columns):
+    dataset.drop(columns, axis = 1, inplace = True)
+    
+        
+    
 raw_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
 train_data = raw_data.copy(deep = True)
 
-train_data = modify_data(train_data)
-test_data = modify_data(test_data)
-    #dataset['LotFrontage','MasVnrArea','GarageYrBlt'] = 
-# Features with large missing data :  LotFrontage, FireplaceQu
+data = [train_data,test_data]
+for dataset in data:
+    modify_data(dataset)
 
-drop_column = ['Id','Alley', 'PoolQC', 'Fence', 'MiscFeature']
-train_data.drop(drop_column, axis = 1, inplace = True)
-train_data = train_data.dropna(axis = 0 , how='all')
-    
-null = train_data.isnull().sum()                                    #Remove Columns 'Alley', 'PoolQC', 'Fence', 'MiscFeature'
-nullColumns = null[null > 0].index.values.tolist()
-describe = train_data[nullColumns].describe()                       # Only 3 columns to be filed with int values inplace of na
-intNullColumns = describe.columns.values.tolist()
-    
-for col in intNullColumns:
-    dataset[col].fillna(dataset[col].mean(),inplace = True)         #Integer null columns with mean
-    nullColumns.remove(col)
-for col in nullColumns:
-    dataset[col].fillna(dataset[col].mode(),inplace = True) 
+#correlation = raw_data.corr()
+#leastCorrelation = correlation[correlation['SalePrice'] < 0].index.values.tolist()
+#leastCorrelation
+nunique = train_data.nunique()
+nuniqueCol = nunique[nunique < 100].index.values.tolist()
+
+for dataset in data:
+    remLeastUniqueColumns(dataset,nuniqueCol)
